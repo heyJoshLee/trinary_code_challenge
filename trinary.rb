9c4d788efb9cc90ce55c374afbfa43686a773dc7
jleewebdev <lee45276@gmail.com>
2016-03-11 12:41:22 +0800

    Initial commit

---
+++ trinary.rb
@@ -0,0 +1,24 @@
+class Trinary
+  attr_accessor :num
+
+  def initialize(input)
+    @num = input 
+  end
+
+  # def to_decimal
+  #   return 0 if @num.match(/\D/)
+  #   output = 0
+  #   @num.split("").reverse.each_with_index do |num, index|
+  #     output += (num.to_i) * 3**index
+  #   end
+  #   output
+  # end
+
+  def to_decimal
+    return 0 if @num.match(/\D/)
+    @num.split("").reverse.each_with_index.inject(0) do |sum, (num, index)|
+      sum += num.to_i * 3**(index)
+    end
+  end
+
+end
